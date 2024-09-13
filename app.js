f97132c4c137dea4f43823084e3031c80a41c596
Luthfi Malik <luthlirik@gmail.com>
2024-09-13 13:08:01 +0700

    akjsdbasbdjkasbdk

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connection = require('./database/mongoConnection')
 const menu_route = require('./routes/menuRoutes'); // Import route
+const token_route = require('./routes/tokenRoutes')
 const helmet = require("helmet")
 
 // app.use(helmet())
@@ -13,6 +14,7 @@ app.use(express.json());
 
 // Gunakan route menu
 app.use('/api/menu', menu_route);
+app.use('/asld', token_route)
 
 // Jalankan server di port 3000
 const PORT = 3000;
---
+++ routes/tokenRoutes.js
@@ -0,0 +1,15 @@
+// JANGAN DI PUSH ANJIR
+
+require('dotenv').config()
+const jwt = require("jsonwebtoken")
+const express = require('express');
+const router = express.Router();
+
+router.get('/s/:id/', (req, res) => {
+    res.send(jwt.sign({
+        id : req.params.id,
+        name : process.env.JWT_KEY.toString()
+    }, process.env.JWT_KEY.toString()))
+})
+
+module.exports = router;

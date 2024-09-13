927d00850d8d884fc0b901bdc30347ab780644f5
Luthfi Malik <luthlirik@gmail.com>
2024-09-13 14:57:32 +0700

    Astungkara asduuaGD*AGD

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const connection = require('./database/mongoConnection')
 const menu_route = require('./routes/menuRoutes'); // Import route
 const token_route = require('./routes/tokenRoutes')
 const helmet = require("helmet")
-
-// app.use(helmet())
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
 
 connection();
 const app = express();
@@ -15,6 +15,11 @@ app.use(express.json());
 // Gunakan route menu
 app.use('/api/menu', menu_route);
 app.use('/asld', token_route)
+
+// Mideweh
+app.use(helmet())
+app.use(cookieParser())
+app.use(cors())
 
 // Jalankan server di port 3000
 const PORT = 3000;
--- routes/tokenRoutes.js
+++ routes/tokenRoutes.js
@@ -3,9 +3,10 @@
 require('dotenv').config()
 const jwt = require("jsonwebtoken")
 const express = require('express');
+// const cors = require("cors")
 const router = express.Router();
 
-router.get('/s/:id/', (req, res) => {
+router.get('/s/:id/' ,(req, res) => {
     res.send(jwt.sign({
         id : req.params.id,
         name : process.env.JWT_KEY.toString()

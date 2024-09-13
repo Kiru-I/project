0fb209eac2279625d021a086663cfbe66d268b8a
Kiru-I <julianbenhart@gmail.com>
2024-09-13 12:44:12 +0700

    nyoba

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const express = require('express');
-const connection = require('./database/mongoConnection')
-const route = require('./routes/menuRoutes'); // Import route
+const connection = require('./database/mongoConnection');
+const route = require('./routes/beritaRoutes'); // Import route
+const authRoutes = require('./middleware/authjwt'); // Import auth routes
 
 connection();
 const app = express();
 
-// Middleware untuk parsing JSON
+// Middleware for parsing JSON
 app.use(express.json());
 
+// Public routes (register and login don't require JWT authentication)
+app.use('/api', authRoutes);
 
-// Gunakan route menu
-app.use('/api', route);
+// Protected routes (apply JWT middleware to only these routes)
+app.use('/api/berita', require('./middleware/jwtMiddleware'), route);
 
-// Jalankan server di port 3000
+// Start the server
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server berjalan di port ${PORT}`);
